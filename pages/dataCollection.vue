95198d776bb55a382342a58e4f492b8a4537d266
unknown <dudals2912@gmail.com>
2023-01-09 14:03:26 +0900

    first commit

---
+++ pages/dataCollection.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <AppAlert>
+      This is an auto-improted component
+    </AppAlert>
+ </div>
+</template>
+
+
+
+
+<!--
+  <template>
+    <v-row>
+      <v-col class="text-center">
+        <img src="/NI.png"  alt="Vuetify.js" class="mb-5" width="150px" height="130px"/>
+        <blockquote class="blockquote">
+          &#8220;NIDAQ Data Colletion&#8221;
+         <v-col class ="text-center">
+          <v-btn color="green"> Start </v-btn>
+          </v-col>
+        </blockquote>  
+      </v-col>
+    </v-row>
+ </template>
+-->
